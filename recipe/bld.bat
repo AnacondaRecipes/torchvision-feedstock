8176f428dab2698b815592b250424af2f0c38779
Daniel Petry <dpetry@anaconda.com>
2024-06-25 15:05:11 -0500

    point win build system towards .lib files; whitelist the files that aren't being found, same as with pytorch; use normal jpeg as we have only got libjpeg-turbo for mac (they are api compatible)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,4 @@
 set "TORCHVISION_INCLUDE=%LIBRARY_INC%"
+:: point the build system towards the .lib files
+set "TORCHVISION_LIBRARY=%PREFIX%\\Lib\\site-packages\\torch\\lib"
 %PYTHON% -m pip install . -vv --no-deps --no-build-isolation
