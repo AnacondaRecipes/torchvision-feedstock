57eb4e28580f4e8d8713ad6eaf21b1052e18cd5b
Daniel Petry <dpetry@anaconda.com>
2024-06-25 15:21:27 -0500

    whitelist for linux too; turn on debug printing for win batch

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,6 @@
-set "TORCHVISION_INCLUDE=%LIBRARY_INC%"
+@echo On
+
+set TORCHVISION_INCLUDE=%LIBRARY_INC%
 :: point the build system towards the .lib files
-set "TORCHVISION_LIBRARY=%PREFIX%\\Lib\\site-packages\\torch\\lib"
+set TORCHVISION_LIBRARY=%PREFIX%\\Lib\\site-packages\\torch\\lib
 %PYTHON% -m pip install . -vv --no-deps --no-build-isolation
