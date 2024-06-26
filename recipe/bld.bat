006f549f01cba4775ecc1ccc00b1bb4b488ed64d
Daniel Petry <dpetry@anaconda.com>
2024-06-26 15:45:02 -0500

    disable some tests, fiddle with win configuration for test purposes

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,5 +4,6 @@ set TORCHVISION_INCLUDE=%LIBRARY_INC%
 :: point the build system towards the .lib files
 :: set TORCHVISION_LIBRARY=%PREFIX%\\Lib\\site-packages\\torch\\lib
 set LIB=%PREFIX%\\Lib\\site-packages\\torch\\lib;%LIB%
-set CMAKE_LIBRARY_PATH=%PREFIX%\\Lib\\site-packages\\torch\\lib;%CMAKE_LIBRARY_PATH%
+:: if it fails to find libraries, uncomment below and comment above
+::set CMAKE_LIBRARY_PATH=%PREFIX%\\Lib\\site-packages\\torch\\lib;%CMAKE_LIBRARY_PATH%
 %PYTHON% -m pip install . -vv --no-deps --no-build-isolation
