0fb0433f8c94c9935c91d4527ee7a1a088efe460
Daniel Petry <dpetry@anaconda.com>
2024-07-12 17:56:03 -0500

    provide ptx for older archs to give better compatibility

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,21 +3,22 @@ set -ex
 if [[ "$cuda_compiler_version" == "None" ]]; then
   export FORCE_CUDA=0
 else
+  # +PTX should go to the oldest arch. There's a modest runtime performance
+  # hit for (unlisted) newer arches on doing this, but the benefit is wider
+  # compatibility.
   if [[ ${cuda_compiler_version} == 11.2* ]]; then
-      export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6"
+      export TORCH_CUDA_ARCH_LIST="3.5+PTX;5.0;6.0;6.1;7.0;7.5;8.0;8.6"
   elif [[ ${cuda_compiler_version} == 11.8 ]]; then
-      export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9"
+      export TORCH_CUDA_ARCH_LIST="3.5+PTX;5.0;6.0;6.1;7.0;7.5;8.0;8.6;9.0"
       export CUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME
   elif [[ ${cuda_compiler_version} == 12.0 ]]; then
-      export TORCH_CUDA_ARCH_LIST="5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0"
+      export TORCH_CUDA_ARCH_LIST="5.0+PTX;6.0;6.1;7.0;7.5;8.0;8.6;9.0"
       # $CUDA_HOME not set in CUDA 12.0. Using $PREFIX
       export CUDA_TOOLKIT_ROOT_DIR="${PREFIX}"
   else
       echo "unsupported cuda version. edit build.sh"
       exit 1
   fi
-  # Add PTX at the end, always
-  export TORCH_CUDA_ARCH_LIST="${TORCH_CUDA_ARCH_LIST}+PTX"
   export FORCE_CUDA=1
 fi
 
