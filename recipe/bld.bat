97384c9e966dc6a1b03c05883700710e95ab0d5f
Daniel Petry <dpetry@anaconda.com>
2025-04-16 19:14:58 -0500

    handle NVJPEG in win same way as in unix

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,12 @@
 @echo On
 
+if "%cuda_compiler_version%" == "None" (
+  set FORCE_CUDA=0
+) else (
+  set FORCE_CUDA=1
+)
+set TORCHVISION_USE_NVJPEG=%FORCE_CUDA%
+
 set TORCHVISION_INCLUDE=%LIBRARY_INC%
 :: point the build system towards the .lib files
 set LIB=%PREFIX%\\Lib\\site-packages\\torch\\lib;%LIB%
