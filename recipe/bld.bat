f28209aa0423ff39097308e1b72f298f7f9cc91a
xkong-anaconda <xkong@anaconda.com>
2026-04-02 17:46:56 -0500

    Start cuda130 arch from 7.5 (nvcc 13.1 on Windows dropped 7.0/Volta)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,7 +5,7 @@ if "%gpu_variant%" neq "cuda" (
 ) else (
   set FORCE_CUDA=1
   :: CUDA 13.x: dropped compute_50-61, min is 7.0 (Volta)
-  set TORCH_CUDA_ARCH_LIST=7.0;7.5;8.0;8.6;8.9;9.0;10.0;10.3;12.0;12.1+PTX
+  set TORCH_CUDA_ARCH_LIST=7.5;8.0;8.6;8.9;9.0;10.0;10.3;12.0;12.1+PTX
 )
 set TORCHVISION_USE_NVJPEG=%FORCE_CUDA%
 
