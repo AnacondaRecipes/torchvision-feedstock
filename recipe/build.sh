c6a760dbe4960f5ed5e44cb6ab7bfe6026903d4f
xkong-anaconda <xkong@anaconda.com>
2026-05-05 12:33:23 -0500

    Review fixes: drop speculative py3.14 test skips, fix version comments, add patch-drop notes

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@ set -ex
 if [[ "${gpu_variant}" != "cuda" ]]; then
   export FORCE_CUDA=0
 else
-  # CUDA 12.8: cap at 10.0 (pytorch 2.9.1 doesn't support arch 10.1)
+  # CUDA 12.8: cap at 10.0 (pytorch 2.10.0 doesn't support arch 10.1)
   export TORCH_CUDA_ARCH_LIST="5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0;10.0+PTX"
   export FORCE_CUDA=1
 fi
